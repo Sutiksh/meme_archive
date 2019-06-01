b4a2faafc8f4895b7e916747e8a528d06c40f1b8
Sutiksh Verma <verma.sutiksh27@gmail.com>
2019-06-01 14:48:58 +0530

    images and some cahnges in ui

--- lib/dev_meme.dart
+++ lib/dev_meme.dart
@@ -17,6 +17,38 @@ class _DevMemeState extends State<DevMeme> {
       ),
       body: Container(
         color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.builder(
+            itemCount: 50,
+            itemBuilder: (BuildContext context, int pos) {
+              return Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: new Container(
+                  width: 280,
+                  height: 380,
+                  color: Colors.white,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        child: Image.asset("meme.jpg",
+                          fit: BoxFit.cover,
+                          height: ,),
+                      ),
+                      Container(
+                        child: ListTile(
+                          leading: Image.asset("like.png",
+                          width: 35,
+                          height: 35,),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
